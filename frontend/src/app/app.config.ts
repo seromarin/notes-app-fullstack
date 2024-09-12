4e77471a80bb8002ed4e7bcc84ea009e2df575ce
Sebastian Rodriguez Marin <rodriguez.marin.sebastian@gmail.com>
2024-09-11 21:50:04 -0500

    feat: add angular material

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,6 +1,11 @@
 import { type ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideClientHydration(), 
+    provideAnimationsAsync(),
+  ]
 };
